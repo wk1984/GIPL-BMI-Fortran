c21d7a56981f1c4a52bcfbb12fd0d9580f23150f
wk1984 <waccount1984@gmail.com>
2019-06-06 15:42:34 -0600

    Update main.f90

--- GIPL/main.f90
+++ GIPL/main.f90
@@ -1,58 +1,38 @@
 program gipl2
 
-use gipl_model
-type (gipl_model_type) :: model
+    use gipl_model
+    type (gipl_model_type) :: model
 
-real*8 :: run_time_start, run_time_final
+    real*8 :: run_time_start, run_time_final
 
-character*164 fconfig
+    character*164 fconfig
 
-real, allocatable :: x(:), y(:)
+    real, allocatable :: x(:), y(:)
 
-call cpu_time(run_time_start)
+    call cpu_time(run_time_start)
 
-fconfig = '../data/gipl_config.cfg'
+    fconfig = '../data/gipl_config.cfg'
 
-!call system('pwd')
+    call initialize(model, fconfig)
 
-call initialize(model,fconfig)
-call run_model
-!call finalize
+    allocate(x(model % n_grd))
+    allocate(y(model % n_grd))
+    !
+    x = model % zdepth
+    y = model % temp(1,:)
+    
+    ! temp(1,40): soil surface temperature.
+    
+    print*, model % top_run_time, model % temp(1, 40)
 
-!print*, model%n_time
-!print*, model%n_grd
-!
-!do i = 1, model%n_grd
-!!    print*, model%zdepth(i), model%temp(1,i)
-!enddo
-!
-allocate(x(model%n_grd))
-allocate(y(model%n_grd))
-!
-x = model%zdepth
-y = model%temp(1,:)
+    do i = 1, model % n_time -1
 
-!print*, model%temp(1,40)
-!print*, model%RES(1,2)
+        call update(model)
 
-do i = 1, 1
+        print*, model % top_run_time, model % temp(1, 40)
 
-!call update(model)
+    enddo
 
-!print*, i, model%temp(1,40)
-!print*, model%RES(1,4)
-
-enddo 
-!
-!x = model%zdepth
-!y = model%temp(1,:)
-
-!do i = 1, model%n_time
-!!   print*, modelR%ES(i,4)
-!enddo
-
-!print*, model%temp(1,41)
-
-call finalize(model)
+    call finalize(model)
 
 end ! end of main program
