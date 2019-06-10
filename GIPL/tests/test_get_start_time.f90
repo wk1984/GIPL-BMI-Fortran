c931d623fb8e76f417be272e8b130fb32464d7db
wk1984 <waccount1984@gmail.com>
2019-06-10 14:31:03 -0600

    Update test_get_start_time.f90

--- GIPL/tests/test_get_start_time.f90
+++ GIPL/tests/test_get_start_time.f90
@@ -10,12 +10,24 @@ program test_get_start_time
 
   type (bmi_gipl) :: m
   double precision :: start_time
-
+  
   status = m%initialize(config_file)
   status = m%get_start_time(start_time)
   status = m%finalize()
-
-  if (start_time.ne.expected_time) then
+  
+  if (status .eq. 0) then
+  
+  print*, start_time, expected_time
+	
+  if (start_time .ne. expected_time) then
      stop BMI_FAILURE
   end if
+  
+  else
+  
+  stop BMI_FAILURE
+  
+  endif
+
+  
 end program test_get_start_time
