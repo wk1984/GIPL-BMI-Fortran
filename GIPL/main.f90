691793df0a2c8f62a7c0f5400e1aae6ad0574ecc
wk1984 <waccount1984@gmail.com>
2019-06-10 17:06:00 -0600

    Update main.f90
    refomate code

--- GIPL/main.f90
+++ GIPL/main.f90
@@ -12,33 +12,33 @@ program gipl2
     call cpu_time(run_time_start)
 
     model%write_outputs_or_not = 1
-    
-    fconfig = 'gipl_config.cfg'
 
-    call initialize(model, fconfig)   
+    fconfig = 'test.cfg'
+
+    call initialize(model, fconfig)
 
     allocate(x(model % n_grd))
     allocate(y(model % n_grd))
     !
     x = model % zdepth
-    y = model % temp(1,:)
-    
+    y = model % temp(1, :)
+
     ! temp(1,40): soil surface temperature.
-    
-!     model%vwc(1,1) = 0.1
-        
+
+    !     model%vwc(1,1) = 0.1
+
     do i = 1, 365
-        
+
         cur_time = model%top_run_time
-        
-!        print*, model%utemp(3,1)
-        
-!        model%utemp(3,1) = 0
-!        model%snd(:,1)   = 0
-!        model%stcon(:,1)  = 0.3
-        
-!        print*, model%utemp(3,1)
-        
+
+        !        print*, model%utemp(3,1)
+
+        !        model%utemp(3,1) = 0
+        !        model%snd(:,1)   = 0
+        !        model%stcon(:,1)  = 0.3
+
+        !        print*, model%utemp(3,1)
+
         call update(model)
 
         print*, cur_time, model % temp(1, 40)
@@ -47,4 +47,5 @@ program gipl2
 
     call finalize(model)
 
-end ! end of main program
+end
+! end of main program
