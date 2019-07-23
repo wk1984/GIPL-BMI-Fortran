e689e68d0b55b1ee51ce771d586c3d24937f2289
wk1984 <waccount1984@gmail.com>
2019-07-23 11:32:52 -0600

    Update test_initialize.f90

--- GIPL/tests/test_initialize.f90
+++ GIPL/tests/test_initialize.f90
@@ -2,17 +2,15 @@ program test_initialize
 
   use bmif_1_2, only: BMI_FAILURE
   use bmigiplf
-  use fixtures, only: status
+  use fixtures, only: status, config_file
 
   implicit none
-
-  character (len=256), parameter :: config_file1 = "test.cfg"
 
   type (bmi_gipl) :: m
   integer :: status1, status2, s
   real*8 :: end_time
 
-  status1 = m%initialize(config_file1)
+  status1 = m%initialize(config_file)
   status = m%finalize()
     
   if (status1 .ne. 0) then
