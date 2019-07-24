7ce29d8a45da57604c40ac62c2fbc01e080d3a89
wk1984 <waccount1984@gmail.com>
2019-07-24 11:37:05 -0600

    Update vargrid_ex.f90
    fix a bug in example vargrid_ex, because of the non-uniform grid space in soil column.

--- GIPL/examples/vargrid_ex.f90
+++ GIPL/examples/vargrid_ex.f90
@@ -10,12 +10,14 @@ program vargrid_test
   character (len=BMI_MAX_VAR_NAME), pointer :: names(:)
   integer :: grid_id
   character (len=BMI_MAX_VAR_NAME) :: astring
+  character (len=BMI_MAX_VAR_NAME), parameter :: config_file = "test.cfg"
   integer :: asize
-  double precision, dimension(2) :: darray
-  integer, dimension(2) :: iarray
+  double precision, dimension(3) :: darray
+  integer, dimension(3) :: iarray
+  double precision, dimension(176) :: darray2
 
   write (*,"(a)",advance="no") "Initializing..."
-  s = m%initialize("test.cfg")
+  s = m%initialize(config_file)
   write (*,*) "Done."
 
   s = m%get_output_var_names(names)
@@ -30,16 +32,16 @@ program vargrid_test
   s = m%get_grid_type(grid_id, astring)
   write (*,"(a, 1x, a)") "Grid type:", trim(astring)
   s = m%get_grid_origin(grid_id, darray)
-  write (*,"(a, 1x, 2(f8.2))") "Grid origin:", darray
+  write (*,"(a, 1x, 3(f8.2))") "Grid origin:", darray
   s = m%get_grid_rank(grid_id, asize)
   write (*,"(a, i3)") "Grid rank:", asize
   s = m%get_grid_shape(grid_id, iarray)
-  write (*,"(a, 2(1x, i3))") "Grid shape:", iarray
+  write (*,"(a, 3(1x, i3))") "Grid shape:", iarray
   s = m%get_grid_size(grid_id, asize)
   write (*,"(a, i8)") "Grid size:", asize
-  s = m%get_grid_spacing(grid_id, darray)
-  write (*,"(a, 1x, 2(f8.2))") "Grid spacing:", darray
-
+  s = m%get_grid_spacing(grid_id, darray2)
+  write (*,"(a, 1x, 5(f8.4))") "Grid spacing:", darray2(1:5)
+  
   s = m%get_var_itemsize(names(1), asize)
   write (*,"(a, i8, 1x, a)") "Item size:", asize, "bytes"
   s = m%get_var_nbytes(names(1), asize)
